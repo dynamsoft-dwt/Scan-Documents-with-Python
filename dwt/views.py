539a167a3e07f022506fb9b59f501e469a252364
Xiao Ling <lingxiao1002@gmail.com>
2021-09-15 17:03:15 +0800

    Upload files

--- dwt/views.py
+++ dwt/views.py
@@ -3,6 +3,20 @@ from django import template
 from django.shortcuts import render
 import os
 
-# Create your views here.
 def index(request):
   return render(request, 'dwt/index.html')
+
+def upload(request):
+    if request.method == 'POST':
+        handle_uploaded_file(request.FILES['RemoteFile'], str(request.FILES['RemoteFile']))
+        return HttpResponse("Successful")
+
+    return HttpResponse("Failed")
+
+def handle_uploaded_file(file, filename):
+    if not os.path.exists('upload/'):
+        os.mkdir('upload/')
+
+    with open('upload/' + filename, 'wb+') as destination:
+        for chunk in file.chunks():
+            destination.write(chunk)
